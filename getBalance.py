69ae1dfac1e51d3d2ad32e38b317098c26fad6c3
Nicholas Pai <npai.nyc@gmail.com>
2018-09-26 11:14:58 -0400

    error checking done

--- getBalance.py
+++ getBalance.py
@@ -13,24 +13,33 @@ minter = validator
 # Return 1 if balance has increased, -1 if decreased, 0 if unchanged
 def getBalance(address):
 	latestBlock = w3.eth.blockNumber
-	oldBalance = w3.fromWei(w3.eth.getBalance(address, block_identifier=latestBlock-1), 'ether')
-	newBalance = w3.fromWei(w3.eth.getBalance(address), 'ether')
 	try:
-		if newBalance < oldBalance:
-			result = ({'delta':-1, 'new': newBalance, 'old': oldBalance})
-			return result
-		elif newBalance > oldBalance:
-			result = ({'delta': 1, 'new': newBalance, 'old': oldBalance})
-			return result
-		else:
-			result = ({'delta': 0, 'new': newBalance, 'old': oldBalance})
-			return result
+		oldBalance = w3.fromWei(w3.eth.getBalance(address, block_identifier=latestBlock-1), 'ether')
 	except:
-		print('ERROR reading balances')
+		print('ERROR reading oldBalance')
+		return
+	try:
+		newBalance = w3.fromWei(w3.eth.getBalance(address), 'ether')
+	except:
+		print('ERROR reading newBalance')
+		return
+
+	if newBalance < oldBalance:
+		result = ({'delta':-1, 'new': newBalance, 'old': oldBalance})
+		return result
+	elif newBalance > oldBalance:
+		result = ({'delta': 1, 'new': newBalance, 'old': oldBalance})
+		return result
+	else:
+		result = ({'delta': 0, 'new': newBalance, 'old': oldBalance})
+		return result
+
 
 # Text recipients if owner balance has changed
 def textBalanceOwner(recipients):
 	deltaBalance = getBalance(owner)
+	if deltaBalance is None:
+		return
 	latestBlock = w3.eth.blockNumber
 	blockNumber = str('Block number: {}\n'.format(latestBlock))
 	if deltaBalance['delta'] == 1:
@@ -47,6 +56,8 @@ def textBalanceOwner(recipients):
 # Text recipients if validator balance has changed
 def textBalanceValidator(recipients):
 	deltaBalance = getBalance(validator)
+	if deltaBalance is None:
+		return
 	latestBlock = w3.eth.blockNumber
 	blockNumber = str('Block number: {}\n'.format(latestBlock))
 	if deltaBalance['delta'] == 1:
