70639b1d57265f73af2a0d01af843d66c33038e4
Nicholas Pai <npai.nyc@gmail.com>
2018-09-26 10:21:27 -0400

    text Nick if balance changes

--- getBalance.py
+++ getBalance.py
@@ -11,24 +11,55 @@ validator = '0xFE6EB9835041e16A67eeEEcdb7945aF27Bc5a28D'
 # Minter can mint new CUSD
 minter = validator
 
-# Balances
-ownerBalance = w3.eth.getBalance(owner)
-validatorBalance = w3.eth.getBalance(validator)
-
-# Balances using 18 decimals
-ownerBalance_eth = w3.fromWei(ownerBalance, 'ether')
-validatorBalance_eth = w3.fromWei(validatorBalance, 'ether')
-
-def textBalance():
+# Return 1 if balance has increased, -1 if decreased, 0 if unchanged
+def getBalance(address):
 	latestBlock = w3.eth.blockNumber
-	ownerBalance = str("Owner has {} ETH\n".format(ownerBalance_eth))
-	validatorAndMinterBalance = str("Minter&Validator has {} ETH\n".format(validatorBalance_eth))
+	oldBalance = w3.fromWei(w3.eth.getBalance(address, block_identifier=latestBlock-1), 'ether')
+	newBalance = w3.fromWei(w3.eth.getBalance(address), 'ether')
+	if newBalance < oldBalance:
+		result = ({'delta':-1, 'new': newBalance, 'old': oldBalance})
+		return result
+	elif newBalance > oldBalance:
+		result = ({'delta': 1, 'new': newBalance, 'old': oldBalance})
+		return result
+	else:
+		result = ({'delta': 0, 'new': newBalance, 'old': oldBalance})
+		return result
+
+def textBalanceOwner():
+	deltaBalance = getBalance(owner)
+	latestBlock = w3.eth.blockNumber
 	blockNumber = str('Block number: {}\n'.format(latestBlock))
-	text(ownerBalance + validatorAndMinterBalance + blockNumber)
+	if deltaBalance['delta'] == 1:
+		latestBlock = w3.eth.blockNumber
+		ownerBalance = str("balance now {} ETH, old balance was {}\n".format(deltaBalance['new'], deltaBalance['old']))
+		text('Owner ETH balance increased\n' + ownerBalance + blockNumber)
+	elif deltaBalance['delta'] == -1:
+		latestBlock = w3.eth.blockNumber
+		ownerBalance = str("balance now {} ETH, old balance was {}\n".format(deltaBalance['new'], deltaBalance['old']))
+		text('Owner ETH balance decreased\n' + ownerBalance + blockNumber)
+	else:
+		print('Owner ETH balance unchanged' + blockNumber)
+
+def textBalanceValidator():
+	deltaBalance = getBalance(validator)
+	latestBlock = w3.eth.blockNumber
+	blockNumber = str('Block number: {}\n'.format(latestBlock))
+	if deltaBalance['delta'] == 1:
+		latestBlock = w3.eth.blockNumber
+		ownerBalance = str("balance now {} ETH, old balance was {}\n".format(deltaBalance['new'], deltaBalance['old']))
+		text('Validator&Minter ETH balance increased\n' + ownerBalance + blockNumber)
+	elif deltaBalance['delta'] == -1:
+		latestBlock = w3.eth.blockNumber
+		ownerBalance = str("balance now {} ETH, old balance was {}\n".format(deltaBalance['new'], deltaBalance['old']))
+		text('Validator&Minter ETH balance decreased\n' + ownerBalance + blockNumber)
+	else:
+		print('Validator&Minter ETH balance unchanged' + blockNumber)
 
 def loop_main(poll_interval):
 	while True:
-		textBalance()
+		textBalanceOwner()
+		textBalanceValidator()
 		time.sleep(poll_interval)
 
 def main():
