11bbc8eec50e06f6b307edd259b3f51109cf8fb5
Samuel Trautwein <samueltrautwein@Samuels-MacBook-Pro.local>
2018-09-26 02:46:23 -0400

    Not pretty but works

--- setupWeb3.py
+++ setupWeb3.py
@@ -9,7 +9,7 @@ import settings
 import os
 
 # Load constants from .env which are now present as system environment variable
-#INFURA_API_KEY_MAIN = os.getenv("INFURA_API_KEY_MAIN")
-INFURA_API_KEY_MAIN="5a4da9d0fd8545fdb1d61a8b26dfd91e"
+INFURA_API_KEY_MAIN = os.getenv("INFURA_API_KEY_MAIN")
+
 # Set provider using Infura node
 w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/" + INFURA_API_KEY_MAIN))
