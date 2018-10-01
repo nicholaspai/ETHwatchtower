55a9d44d9d8bef297d56cdc229626e4721390c93
Nicholas Pai <npai.nyc@gmail.com>
2018-10-01 16:20:03 -0400

    test print

--- __init__.py
+++ __init__.py
@@ -1,17 +1,18 @@
 from getBalance import textBalanceOwner, textBalanceValidator
 import whitelist
 import time
+import os
 
 def loop_main(poll_interval):
 	# Who will receive the alerts
 	recipients = [whitelist.NICK]
+	print('INFURA key: {}'.format(os.environ.get("INFURA_API_KEY_MAIN")))
 	while True:
 		textBalanceOwner(recipients)
 		textBalanceValidator(recipients)
 		time.sleep(poll_interval)
 
 def main():
-	print('INFURA key: {}'.format(os.environ.get("INFURA_API_KEY_MAIN")))
 	loop_main(2)
 
 main()
